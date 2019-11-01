b01bdc2a3173f68d8d6b8fda303c7bdba53edb89
Sergey Strokatov <strokatovsergey@gmail.com>
2019-11-01 16:56:51 +0400

    upgrade getting data from API

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,58 +1,56 @@
 import React, { Component } from 'react';
 
+import SwapiService from '../../services/swapi-service';
+
 import './random-planet.css';
-import SwapiService from  '../../services/swapi-service';
 
 export default class RandomPlanet extends Component {
-  constructor(){
-    super()
-    this.updatePLanet()
-  }
 
   swapiService = new SwapiService();
 
   state = {
-    pictureId: null,
-    population: null,
-    name: null,
-    rotationPeriod: null,
-    duameter: null
-  }
-  
+    planet: {}
+  };
 
-  updatePLanet(){
-    const randomNum = Math.floor(Math.random() * 20 + 2);
-    this.swapiService.getPlanet(randomNum)
-      .then((planet)=>{
-        this.setState({
-          pictureId: randomNum,
-          population: planet.population,
-          name: planet.name,
-          rotationPeriod: planet.rotation_period,
-          duameter: planet.duameter
-        })
-      })
+  constructor() {
+    super();
+    this.updatePlanet();
+  }
+
+  onPlanetLoaded = (planet) => {
+    this.setState({ planet });
+  };
+
+  updatePlanet() {
+    const id = 12;
+    this.swapiService
+      .getPlanet(id)
+      .then(this.onPlanetLoaded);
   }
 
   render() {
 
-    const { population, name, rotationPeriod, duameter, pictureId } = this.state;
+    const { planet: { id, name, population,
+      rotationPeriod, diameter } } = this.state;
 
     return (
       <div className="random-planet jumbotron rounded">
         <img className="planet-image"
-          src={`https://starwars-visualguide.com/assets/img/planets/${pictureId}.jpg`}/>
+             src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
         <div>
-          <h4>Planet Name {name}</h4>
+          <h4>{name}</h4>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
-              <span className="term">Population {population}</span>
+              <span className="term">Population</span>
+              <span>{population}</span>
             </li>
             <li className="list-group-item">
-              <span className="term">Rotation Period {rotationPeriod}</span>
+              <span className="term">Rotation Period</span>
+              <span>{rotationPeriod}</span>
             </li>
             <li className="list-group-item">
-              <span className="term">Diameter {duameter}</span>
+              <span className="term">Diameter</span>
+              <span>{diameter}</span>
             </li>
           </ul>
         </div>
