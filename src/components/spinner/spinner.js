6f681a325c3a4ea5e8c1bb0d4a7f02ad70d8a548
Sergey <s.strokatov@mail.ru>
2019-11-07 22:06:22 +0300

    click name - show person

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -4,7 +4,7 @@ import './spinner.css';
 
 const Spinner = () => {
     return (
-        <div class="lds-css ng-scope">
+        <div className="lds-css ng-scope">
             <div  className="lds-double-ring">
                 <div></div>
                 <div></div>
